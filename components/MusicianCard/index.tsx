b0c3efa780a3d4a039fc91d3170751f61df44d1a
ParkEunwoo <dmsdn960@gmail.com>
2020-04-11 23:37:09 +0900

    use store in musician list #55

--- components/MusicianCard/index.tsx
+++ components/MusicianCard/index.tsx
@@ -2,21 +2,28 @@ import * as React from "react";
 import PlayButton from "./../AudioPlayer/PlayButton";
 import styled from "styled-components";
 
+interface Song {
+  id: string;
+  title: string;
+  date: string;
+  isPlaying: boolean;
+  isLike: boolean;
+  cover_url: string;
+  song_url: string;
+}
+
+interface Musician {
+  id: string;
+  name: string;
+  introduction: string;
+  tags: string[];
+  likes: number;
+  profile_url: string;
+  features: string[];
+  song: Song;
+}
 interface Props {
-  musician: {
-    name: string;
-    introduction: string;
-    tags: string[];
-    likes: number;
-    profile: string;
-    isPlaying: boolean;
-    song: {
-      title: string;
-      cover: string;
-      id: string;
-    };
-    features: string[];
-  };
+  musician: Musician;
 }
 const Card = styled.li`
   width: 392px;
@@ -145,19 +152,19 @@ const Tags = ({ tags }: { tags: string[] }) => {
 const MusicianCard = ({ musician }: Props) => {
   return (
     <Card>
-      <MusicContainer src={musician.song.cover}>
+      <MusicContainer src={musician.song.cover_url}>
         <Space height={40} />
         <PlayButton
           playAudio={() => {}}
           size={64}
-          status={musician.isPlaying}
+          status={musician.song.isPlaying}
         />
         <MusicInfo>{musician.song.title}</MusicInfo>
       </MusicContainer>
       <MusicianInfo>
         <ProfileContainer>
           <Circle>
-            <Profile src={musician.profile} />
+            <Profile src={musician.profile_url} />
           </Circle>
           <Info>
             <Name>{musician.name}</Name>
