bee0ac14861bed99565d3b1856f485a68ffda4ed
JinleeJeong <pjjr0118@gmail.com>
2020-05-07 16:25:22 +0900

    Making Progress Bar folder and finish Layout

---
+++ components/ProgressBar/oneThird.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import styled from 'styled-components';
+
+interface Props {}
+
+const Container = styled.div`
+    margin : 1% 0;
+`;
+
+const ProgressBar = styled.div`
+    width : 100%;
+    height: 8px;
+`;
+const Pro_One = styled.div`
+    width : 33%;
+    float : left;
+    background : linear-gradient(173.96deg, #6865FC 36.28%, #658DFC 100%);
+    border-radius : 0 2px 2px 0;
+    height: 100%;
+`;
+
+const Pro_Two = styled.div`
+    width : 33%;
+    float : left;
+    background : #3E3E41;
+    border-radius : 0 2px 2px 0;
+    box-shadow: 1px 4px 1px rgba(0, 0, 0, 0.25);
+    height: 100%;
+`;
+
+const Pro_Three = styled.div`
+    width : 34%;
+    float : left;
+    background : #313133;
+    border-radius : 0 2px 2px 0;
+    height: 100%;
+`;
+const OneThird = (props: Props) => {
+  return <>
+    
+    <Container>
+        <ProgressBar>
+            <Pro_One/>
+            <Pro_Two/>
+            <Pro_Three/>
+        </ProgressBar>
+    </Container>
+
+    </>
+};
+
+export default OneThird;
