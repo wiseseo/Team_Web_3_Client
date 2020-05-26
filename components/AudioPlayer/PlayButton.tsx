ef3b4951c4e8bf757a342c1d71ca37bb9724f3c9
ParkEunwoo <dmsdn960@gmail.com>
2020-05-26 21:29:13 +0900

    play button change to image #75

--- components/AudioPlayer/PlayButton.tsx
+++ components/AudioPlayer/PlayButton.tsx
@@ -11,12 +11,11 @@ const Button = styled.button`
   border: 1px solid #e2e1e2;
   width: ${({ size }: { size: number }) => size}px;
   height: ${({ size }: { size: number }) => size}px;
-  font-size: ${({ size }: { size: number }) => size / 2.4}px;
   border-radius: 50%;
   background: transparent;
   display: flex;
   justify-content: center;
-  aligns-items: center;
+  align-items: center;
 `;
 
 const Toggle = styled.input.attrs(({ checked }: { checked: boolean }) => ({
@@ -27,57 +26,21 @@ const Toggle = styled.input.attrs(({ checked }: { checked: boolean }) => ({
 }))`
   appearance: none;
   outline: none;
-  width: 1em;
-  height: 1em;
-  position: relative;
-  margin-left: auto;
-  font-size: 1em;
-
-  &:checked ::before {
-    transition: 0.1s;
-    height: 1em;
-    border-left-width: 0.35em;
-    border-top-width: 0em;
-    border-bottom-width: 0em;
-  }
-  &:checked ::after {
-    transition: 0.1s;
-    height: 1em;
-    top: 0;
-    border-left-width: 0.35em;
-    border-top-width: 0em;
-    border-bottom-width: 0em;
-  }
-  &::before {
-    transition: 0.1s;
-    content: "";
-    display: block;
-    height: 0.5em;
-    width: 0px;
-    border-left: 0.5em solid #e2e1e2;
-    border-top: 0.25em solid transparent;
-    border-bottom: 0.25em solid transparent;
-    position: absolute;
-    left: 0;
-  }
-  &::after {
-    transition: 0.1s;
-    content: "";
-    height: 0px;
-    width: 0px;
-    border-left: 0.5em solid #e2e1e2;
-    border-top: 0.25em solid transparent;
-    border-bottom: 0.25em solid transparent;
-    position: absolute;
-    top: 0.25em;
-    right: 0;
+  width: ${({ size }: { size: number }) => size / 2}px;
+  height: ${({ size }: { size: number }) => size / 2}px;
+  background-size: contain;
+  background-position: center center;
+  background-image: url("/static/pause.png");
+  background-repeat: no-repeat;
+  &:checked {
+    background-image: url("/static/play.png");
   }
 `;
 
 const PlayButton = ({ playAudio, size, status }: Props): React.ReactElement => {
   return (
     <Button onClick={playAudio} size={size}>
-      <Toggle checked={status} />
+      <Toggle checked={status} size={size} />
     </Button>
   );
 };
