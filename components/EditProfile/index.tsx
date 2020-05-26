3054b5da08b613c60544b3f84311b2d404818302
JihyeSeo1120 <mansesjh22@gmail.com>
2020-05-26 22:15:15 +0900

    profile page layout #84

--- components/EditProfile/index.tsx
+++ components/EditProfile/index.tsx
@@ -8,12 +8,13 @@ const Contents = styled.div`
     display : flex;
     flex-direction : row;
     background-color : #040104;
-    width L 644px;
+    width :  644px;
     height : 247px;
     color : white;
 `;
 
 const ImgContainer = styled.div`
+    flex: 1;
     background-color : #040104;
     display : flex;
     justify-content : center;
@@ -32,17 +33,129 @@ const Photo = styled.div`
     overflow: hidden;
 `;
 const InfoContainer = styled.div`
+    flex: 1;
+    /*background-color : #040104;*/
+    background-color : white;
+    display : flex;
+    flex-direction : column;
+    justify-content : space-between;
+    align-items : flex-end;
+`;
+
+const EmailContainer = styled.div`
+    width : 320px;
+    height : 32px;
+    background : red;
+    display : flex;
+    flex-direction : row;
+`;
+
+const EmailBox = styled.div`
+    width : 212px;
+    height : 32px;
+    background-color : blue;
+    /*background-color : #040104;*/
+`;
+
+const NickNameContainer = styled.div`
+    width : 320px;
+    height : 32px;
+    background : red;
+    display : flex;
+    flex-direction : row;
+`;
+
+const NickNameBox = styled.div`
+    width : 212px;
+    height : 32px;
+    background-color : blue;
+    /*background-color : #040104;*/
+`;
+
+const NickNameInput = styled.input`
+
+`;
+
+const PhoneNumberContainer = styled.div`
+    width : 320px;
+    height : 32px;
+    background : red;
+    display : flex;
+    flex-direction : row;
+`;
+
+const PhoneNumberBox = styled.div`
+    width : 212px;
+    height : 32px;
+    background-color : blue;
+    /*background-color : #040104;*/
+`;
+
+const PhoneNumberInput = styled.input`
+`;
+
+const Label = styled.label`
+    color : #B3B4BE;
+    font-size : 0.875rem;
+`;
+
+const ModifyBtn = styled.input.attrs({ 
+    type: 'submit',
+    value: '수정'
+  })`
+    color : #6865FC;
+    font-size ; 0.75rem;
     background-color : #040104;
 `;
+const WithdrawalBtn = styled.button`
+    width : 320px;
+    height : 32px;
+    /*background : red;*/
+    background-color : #040104;
+    border: 1px solid #B3B4BE;
+    box-sizing: border-box;
+    border-radius: 0.5rem;
+    color: #B3B4BE;
+    font-size : 0.75rem;
+`;
+
 const EditProfile = (props: Props) => { 
 
     return(
         <Contents>
            <ImgContainer>
-            <Photo/>
+                <Photo/>
            </ImgContainer>
            <InfoContainer>
-
+                <EmailContainer>
+                    <Label>
+                        이메일
+                    </Label>
+                    <EmailBox>
+                        dffdf
+                    </EmailBox>
+                </EmailContainer>
+                <NickNameContainer>
+                    <Label>
+                        닉네임
+                    </Label>
+                    <NickNameBox>
+                        fdfd
+                    </NickNameBox>
+                    <ModifyBtn/>
+                </NickNameContainer>
+                <PhoneNumberContainer>
+                    <Label>
+                        전화번호
+                    </Label>
+                    <PhoneNumberBox>
+                        fsfd
+                    </PhoneNumberBox>
+                    <ModifyBtn/>
+                </PhoneNumberContainer>
+                <WithdrawalBtn>
+                    회원탈퇴
+                </WithdrawalBtn>
            </InfoContainer>
         </Contents>
         
