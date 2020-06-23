02f58df47666793a5dd7d7b1af2b4149f56dbbc2
ParkEunwoo <dmsdn960@gmail.com>
2020-06-23 22:34:19 +0900

    dynamic routing #158

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import MusicianProfile from "../../components/MusicianProfile";
 import MusicianProfileTab from "../../components/MusicianProfileTab";
 
 const Detail: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(id);
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <MusicianProfile></MusicianProfile>
